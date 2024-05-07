4f2cc2fb11c702803814e715bd54ec6a4c4ea8e6
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-05-07 21:03:14 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showAlert(){
+    function tampil(){
+        alert("Segera daftarkan diri anda di event Merangkai bunga pada 2 Mei 2024 sampai 6 Mei 2024 HTM 100K/orang. AYO SEGERA DAFTARKAN DIRI ANDA SEKARANG!!!")
+    }
+    tampil(); // Memanggil fungsi tampil()
+}
